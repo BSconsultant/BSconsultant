485cf3aae5bfd5037a6dffbce0722bd2d5b8932b
aravind <aravindreddyabbu402@gmail.com>
2023-07-24 12:24:17 +0530

    no message

---
+++ admin--ui/src/views/11_product_catalog/AdminProducts.js
@@ -0,0 +1,71 @@
+import React from "react";
+import {useLocation} from "react-router";
+import {example} from "../../data/examData";
+
+import {Link} from "react-router-dom";
+import CustomTable from "../../components/CustomTable";
+import {buttonType} from "../../configuration/configurationUI";
+import Add from "../../components/buttons/Add";
+import {sendRequest} from "../../api/sendRequest";
+import {apis} from "../../configuration/configurationApi";
+import Axios from 'axios';
+
+export default function AdminProducts(props) {
+    const item = useLocation().state;
+
+
+    const [data, setData] = React.useState([]);
+    React.useEffect(() => {
+        Axios.get(apis.base+apis.productList, {
+            headers: {
+                user_id: 7
+            }
+        }).then((succResp) => {
+            callback(succResp);
+        }, (errorresp) => {
+            console.log(JSON.stringify(errorresp));
+        })
+
+    }, []);
+
+    const callback = (res) => {
+        setData(res.data)
+    };
+    const url = window.location.href;
+
+
+    return (
+        <div className="card">
+            <div className="card-header card-header-primary">
+                <div className="row">
+                    <div className="col-md-6">
+                        <h1 className="card-title"><b>Product List</b></h1>
+                    </div>
+                    <div className="col-md-6">
+                        <Add/>
+                    </div>
+                </div>
+            </div>
+
+            <div className="container"><br/>
+                <CustomTable
+                    type={{
+                        "product name": 'product_name',
+                        "product id": "product_id",
+                        "category": "title",
+                        "type": "type",
+                    }}
+                    typeEx={{"Product Image": 'image'}}
+                    style={{}}
+                    searchField={true}
+                    image={["product image"]}
+                    button={{}}
+                    buttonEx={{
+                        "Hide": [buttonType.showOrHide],
+                        "Actions": [buttonType.edit, buttonType.delete, buttonType.Variant]
+                    }}
+                    data={data} api={apis.productList} primaryKey={"product_id"}/>
+            </div>
+        </div>
+    )
+}
---
+++ admin--ui/src/views/11_product_catalog/TrendingSearch.js
@@ -0,0 +1,168 @@
+import React from "react";
+import {useLocation} from "react-router";
+
+import CustomTable from "../../components/CustomTable";
+import {buttonType} from "../../configuration/configurationUI";
+import MultiSelect from "../../components/MultiSelect";
+import {sendRequest} from "../../api/sendRequest";
+import {apis} from "../../configuration/configurationApi";
+import Axios from "axios";
+
+export default function TrendingSearch(props) {
+    const item = useLocation().state;
+    const url = window.location.href;
+    const [product, setProduct] = React.useState([]);
+    const [productSelected, setProductSelected] = React.useState([]);
+    const [data, setData] = React.useState([]);
+    React.useEffect(() => {
+        Axios.get(apis.base+apis.trendingSearchProduct, {
+            headers: {
+                user_id: 7
+            }
+        }).then((succResp) => {
+            callback1(succResp);
+        }, (errorresp) => {
+            console.log(JSON.stringify(errorresp));
+        });
+    }, []);
+
+    const callback1 = (res) => {
+        setProduct(res.data)
+    };
+
+
+    const callback2 = (res) => {
+        setProductSelected(res.data);
+        let result = res.data;
+        let where = [];
+        for (let i = 0; i < result.length; i++) {
+            where.push('Variant_id!=' + result[i].Variant_id)
+        }
+
+        let body = {
+            method: "get",
+            where: where
+        };
+        let formData = new FormData();
+        formData.append("payload", JSON.stringify(body));
+        const res1 = sendRequest(apis.base + apis.trendingSearchProduct, formData, ((res) => callback1(res)), ((err) => console.log(err)));
+    };
+
+    const contain = (item) => {
+        for (let i = 0; i < productSelected.length; i++) {
+            if (productSelected[i].Variant_id === item.Variant_id) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    const submit = () => {
+        for (let i = 0; i < data.length - 1; i++) {
+            let body = {
+                method: "add",
+                data: {
+                    Variant_id: data[i].Variant_id
+                }
+            };
+            let formData = new FormData();
+            formData.append("payload", JSON.stringify(body));
+            const res = sendRequest(apis.base + apis.trendingSearchProductSelected, formData, ((res) => (res)), ((err) => console.log(err)));
+        }
+        let body = {
+            method: "add",
+            data: {
+                Variant_id: data[data.length - 1].Variant_id
+            }
+        };
+        let formData = new FormData();
+        formData.append("payload", JSON.stringify(body));
+        const res1 = sendRequest(apis.base + apis.trendingSearchProductSelected, formData, ((res) => callbackSubmit(res)), ((err) => console.log(err)));
+    };
+
+    const callbackSubmit = () => {
+        window.location.href = url;
+    };
+
+    const selectData = (checked, eachData) => {
+        if (checked) {
+            let d = data;
+            d.push(eachData);
+            setData([...d])
+        } else {
+            setData([...data.filter(item => item !== eachData)])
+        }
+    };
+
+    const selectAll = (checked) => {
+        if (checked) {
+            let dataAdded = data;
+            for (let i = 0; i < product.length; i++) {
+                if (!dataAdded.includes(product[i])) {
+                    dataAdded.push(product[i]);
+                }
+            }
+            setData([...dataAdded]);
+        } else {
+            setData([]);
+        }
+    };
+
+
+    return (
+        <div className="row">
+            <div className="col-5">
+                <div className="card">
+                    <div className="card-header card-header-primary">
+                        <h4 className="card-title">Select Products</h4>
+                    </div>
+                    <div className="card-body">
+                        <div className="row">
+                            <div className="col-md-12">
+                                <MultiSelect
+                                    title={"Select Products Which You have to set on Trending Search"}
+                                    data={product}
+                                    dataSelected={data}
+                                    selectAll={(checkState) => selectAll(checkState)}
+                                    selectData={(checkState, item) => selectData(checkState, item)}
+                                    name={"product_name"}
+                                />
+                            </div>
+                        </div>
+                        <button onClick={submit} type="submit"
+                                className="btn btn-primary pull-center">
+                            Submit
+                        </button>
+
+                    </div>
+                </div>
+            </div>
+            <div className="col-7">
+                <div className="card">
+                    <div className="card-header card-header-primary">
+                        <h4 className="card-title">Selected Products</h4>
+                    </div>
+
+                    <div className="container"><br/>
+                        <CustomTable
+                            type={{
+                                "PRODUCT NAME": 'product_name',
+                            }}
+                            typeEx={{}}
+                            style={{}}
+                            image={["image"]}
+                            button={{
+                                "ACTION": [buttonType.delete],
+                            }}
+                            buttonEx={{}}
+                            searchField={true}
+                            data={productSelected}
+                            primaryKey={"trend_id"}
+                            api={apis.trendingSearchProductSelected}
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
